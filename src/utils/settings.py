42580d35649f397b1106188f0322abbb4bf5c6e3
somath <thmartinezv@unal.edu.co>
2026-03-04 10:56:15 -0500

    add credentials

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -14,6 +14,15 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+# app database
+APP_DB_USER = os.getenv("APP_DB_USER")
+APP_DB_PASS = os.getenv("APP_DB_PASS")
+APP_DB_HOST = os.getenv("APP_DB_HOST")
+APP_DB_PORT = os.getenv("APP_DB_PORT")
+APP_DB_NAME = os.getenv("APP_DB_NAME")
 
 
 if __name__=="__main__":
