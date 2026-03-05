bb0733d95b14b99a98add7017b690550fcd7926e
somath <thmartinezv@unal.edu.co>
2026-03-05 15:11:45 -0500

    Ignore the pydantic validation for now

--- src/api/routers/run_educhat.py
+++ src/api/routers/run_educhat.py
@@ -18,37 +18,40 @@ class RundEduChat(BaseModel):
 
 
 
-class ToolCalls(BaseModel):
-    name : str
-    args : Dict[str, Any]
-    id : str
-    type : str
+# class ToolCalls(BaseModel):
+#     name : str
+#     args : Dict[str, Any]
+#     id : str
+#     type : str
 
 
-class UsageMetadata(BaseModel):
-    input_tokens: int
-    output_tokens: int
-    total_tokens: int
-    input_token_details: Dict[str, Any]
+# class UsageMetadata(BaseModel):
+#     input_tokens: int
+#     output_tokens: int
+#     total_tokens: int
+#     input_token_details: Dict[str, Any] | None = None
+#     output_token_details : Dict[str, Any] | None = None
 
 
-class AiResponse(BaseModel):
-    content : Any
-    tool_calls : ToolCalls
-    usage_metadata : UsageMetadata
+# class AiResponse(BaseModel):
+#     content : Any
+#     tool_calls : List[ToolCalls]
+#     usage_metadata : UsageMetadata
+#     type : str
 
 
-class ToolResponse(BaseModel):
-    content : Any
-    tool_call_id : str
-    name : str
+# class ToolResponse(BaseModel):
+#     content : Any
+#     tool_call_id : str
+#     name : str
+#     type : str
 
 
-EduchatResponseItem = Union[AiResponse, ToolResponse]
+# EduchatResponseItem = Union[AiResponse, ToolResponse]
 
 
 class RunEduChatResponse(BaseModel):
-    educhat_response : List[EduchatResponseItem]
+    educhat_response : List[Dict[str, Any]]
 
 
 
@@ -59,8 +62,10 @@ class RunEduChatResponse(BaseModel):
 @router.post("/", response_model=RunEduChatResponse)
 async def run_educhat_api(body : RundEduChat):
     educhat = config_educhat(user_id=body.user_id, chat_id=body.chat_id)
+    print("A"*80)
     prompt = SYSTEM_PROMPT_4.format(db_skill=DB_SKILL_1)
     agent_response = run_educhat(user_id=body.user_id, chat_id=body.chat_id, human_message=body.human_message, system_prompt=prompt, educhat=educhat)
+    print("B"*80)
     return agent_response
 
 
