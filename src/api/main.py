ddb2a85dfaeae827998780f3477c581fdeaaff16
somath <thmartinezv@unal.edu.co>
2026-03-05 08:07:47 -0500

    add router

--- src/api/main.py
+++ src/api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from src.api.routers import newchats, newuser, runchat
+from src.api.routers import newchats, newuser, runchat, run_educhat
 
 app = FastAPI(title="Chat edubot")
 
 app.include_router(newuser.router)
 app.include_router(newchats.router)
 app.include_router(runchat.router)
-
+app.include_router(run_educhat.router)
 
